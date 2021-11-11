1496e3288e84f9636eff03e4ed3d1e1d29979000
unyuu <unyuu@tenshi.nekomimi.nya>
2021-11-11 15:13:59 +0500

    Добавлен вывод карты

--- Maze-Example-CPP/main.cc
+++ Maze-Example-CPP/main.cc
@@ -21,27 +21,55 @@ const std::string map[10] {
 	"#     #  #",
 	"##########",
 };
-const int
-		MAP_WIDTH = map[0].length(),
-		MAP_HEIGHT = sizeof(map),
+const unsigned
+		MAP_WIDTH = 10,
+		MAP_HEIGHT = 10,
 		START_X = 1,
 		START_Y = 1,
 		FINISH_X = 8,
-		FINISH_Y = 8,
+		FINISH_Y = 8;
+const int
 		EMPTY = 0,
 		WALL = -1,
 		PATH = -2;
 
+void print_map(int work_map[MAP_HEIGHT][MAP_WIDTH])
+{
+	for (unsigned row = 0; row < MAP_HEIGHT; ++row) {
+		for (unsigned col = 0; col < MAP_WIDTH; ++col) {
+			int cell = work_map[row][col];
+			if (cell == WALL) {
+				std::cout << "\u2588\u2588"; // ██
+			} else
+			if (cell == PATH) {
+				std::cout << "\u2591\u2591"; // ░░
+			} else {
+				std::cout << "  ";
+			}
+		}
+		std::cout << std::endl;
+	}
+}
 
+void cook_map(int work_map[MAP_HEIGHT][MAP_WIDTH])
+{
+	for (unsigned row = 0; row < MAP_HEIGHT; ++row)
+		for (unsigned col = 0; col < MAP_WIDTH; ++col) {
+			if (map[row][col] == '#')
+				work_map[row][col] = WALL;
+			else
+				work_map[row][col] = EMPTY;
+		}
+}
 
 int main()
 {
-	int work_map[MAP_WIDTH][MAP_HEIGHT];
+	int work_map[MAP_HEIGHT][MAP_WIDTH] {}; // [строка][столбец]
 
-	for (unsigned row = 0; row < MAP_HEIGHT; ++row)
-		for (unsigned col = 0; col < MAP_WIDTH; ++col) {
+	cook_map(work_map);
+	print_map(work_map);
 
-		}
+
 
 
 
